dac1e3b8733934c9904f9f151d293ce86f4c128f
Yuvraj <94469765+yuv-dev@users.noreply.github.com>
2024-12-09 15:36:54 +0530

    Revert "minor auth update"

--- auth.ts
+++ auth.ts
@@ -1,56 +1,7 @@
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
-import { client } from "./sanity/lib/client"
-import { AUTHOR_BY_GITHUB_ID_QUERY } from "@/sanity/lib/queries"
-import { writeClient } from "@/sanity/lib/write-client"
+import Google from "next-auth/providers/google" 
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [GitHub],
-  callbacks:{
-
-    // signIn
-    async signIn({
-      user:{name, email, image}, 
-      profile:{id, login, bio}
-    }){
-      
-      const existingUser = await client.withConfig({useCdn:false}).fetch(
-        AUTHOR_BY_GITHUB_ID_QUERY, {id:id});
-        if(!existingUser) {
-          await writeClient.create({
-            _type:"author",
-            id :id,
-            name:name,
-            username:login,
-            email:email,
-            image:image,
-            bio:bio || ""
-          })
-        }
-
-        if(existingUser) return true;
-    },
- 
-
-    // // JWT TOKEN
-    // async jwt({token, account, profile}){
-    //   if(!account && profile) {
-    //     const  user = client.withConfig({useCdn:false}).fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
-    //       id: profile?.id
-    //     });
-
-    //     console.log(user)
-
-    //     if(!user) {
-    //       token.id = user?._id;
-    //     }
-    //     return token;
-    //   }
-    // },
-
-    // async session({session, token}){
-    //   Object.assign(session, {id:token.id});
-    //   return session;
-    // }
-  }
+  providers: [GitHub, Google],
 })
