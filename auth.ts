541641c23b3b06e0397ddf15092e20f0b5c3ec48
Uberyuv <yuvraj.light@gmail.com>
2024-12-26 12:59:17 +0530

    X

--- auth.ts
+++ auth.ts
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
-import Google from "next-auth/providers/google" 
+import { client } from "./sanity/lib/client"
+import { AUTHOR_BY_GITHUB_ID_QUERY } from "@/sanity/lib/queries"
+import { writeClient } from "@/sanity/lib/write-client"
 
 export const { handlers,auth, signIn, signOut } = NextAuth({
   providers: [GitHub],
