2ea61aab252b93a7e37619c4e6ccd2750c2cd202
Uberyuv <yuvraj.light@gmail.com>
2024-12-26 12:35:11 +0530

    merge conflict resolved

--- auth.ts
+++ auth.ts
@@ -1,8 +1,6 @@
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
-import { client } from "./sanity/lib/client"
-import { AUTHOR_BY_GITHUB_ID_QUERY } from "@/sanity/lib/queries"
-import { writeClient } from "@/sanity/lib/write-client"
+import Google from "next-auth/providers/google" 
 
 export const { handlers,auth, signIn, signOut } = NextAuth({
   providers: [GitHub],
